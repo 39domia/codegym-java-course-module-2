1a5e7a215d733adb4cad28e769f65463df5bb048
Domia <39hson@gmail.com>
2020-11-19 16:56:05 +0700

    Temp LeapYear clean code

--- 1-Introduction to Java/FindLeapYear/src/LeapYear.java
+++ 1-Introduction to Java/FindLeapYear/src/LeapYear.java
@@ -5,19 +5,23 @@ public class LeapYear {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a year: ");
         int year = scanner.nextInt();
-        if (year % 4 == 0) {
-            if (year % 100 == 0) {
-                if (year % 400 == 0) {
-//                    System.out.printf("%d is a leap year", year);
-                    System.out.println(year + " is a leap year");
-                } else {
-                    System.out.println(year + " is NOT a leap year");
+
+        boolean isLeapYear = false;
+
+        if(year % 4 == 0){
+            if(year % 100 == 0){
+                if(year % 400 == 0){
+                    isLeapYear = true;
                 }
             } else {
-                System.out.println(year + " is a leap year");
+                isLeapYear = true;
             }
+        }
+
+        if(isLeapYear){
+            System.out.printf("%d is a leap year", year);
         } else {
-            System.out.println(year + " is NOT a leap year");
+            System.out.printf("%d is NOT a leap year", year);
         }
     }
 
