30a31ff5c3c5a1f1311a15c17a9890e57214c454
Domia <39hson@gmail.com>
2020-11-19 21:21:14 +0700

    test github desktop

--- 1-Introduction to Java/ConvertNumberToText/src/NumberText.java
+++ 1-Introduction to Java/ConvertNumberToText/src/NumberText.java
@@ -2,10 +2,28 @@ import java.util.Scanner;
 
 public class NumberText {
     public static void main(String[] args) {
-        System.out.println("Enter a number: ");
+        System.out.print("Enter a number: ");
         Scanner scanner = new Scanner(System.in);
-        int input = scanner.nextInt();
+        int number = scanner.nextInt();
+        switch (String.valueOf(number).length()){
+            case 1 -> {
+                switch (number) {
+                    case 1 -> System.out.println("one");
+                    case 2 -> System.out.println("two");
+                    case 3 -> System.out.println("three");
+                    case 4 -> System.out.println("four");
+                    case 5 -> System.out.println("five");
+                    case 6 -> System.out.println("six");
+                    case 7 -> System.out.println("seven");
+                    case 8 -> System.out.println("eight");
+                    case 9 -> System.out.println("night");
+                    default -> System.out.println("out of ability");
+                }
+            }
+            case 2 -> System.out.println("a");
+        }
 
-        System.out.println(String.valueOf(input).length());
+
+        System.out.println(String.valueOf(number).length());
     }
 }
