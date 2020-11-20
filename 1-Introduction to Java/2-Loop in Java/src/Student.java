c104a0d8080c5ef087b12e8c2acca79b04517261
Domia <39hson@gmail.com>
2020-11-20 15:01:13 +0700

    Temp check prime number

---
+++ 1-Introduction to Java/2-Loop in Java/src/Student.java
@@ -0,0 +1,8 @@
+public class Student {
+
+
+    public static void main(String[] args) {
+        System.out.println("Max integer" + Integer.MAX_VALUE);
+        System.out.println("Min integer" + Integer.MIN_VALUE);
+    }
+}
