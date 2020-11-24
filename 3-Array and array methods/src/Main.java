3b757cade4861ff2bf3ac01e00feb30e78ff5213
Domia <39hson@gmail.com>
2020-11-24 15:19:52 +0700

    Done ReverseInputArray

---
+++ 3-Array and array methods/src/Main.java
@@ -0,0 +1,14 @@
+import b.b.Person;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Person son = new Person(20, "Son");
+        System.out.println(son.toString1());
+//        System.out.println(son.display());
+    }
+}
+
