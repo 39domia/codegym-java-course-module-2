3edbcddd5b6945b8ae68d25d6ccd5b37e140ac17
Domia <39hson@gmail.com>
2020-11-25 17:06:29 +0700

    Add TestCircle, Circle

---
+++ 5-Access modifier, static method, static property/src/TestCircle.java
@@ -0,0 +1,7 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle cr = new Circle(5);
+        System.out.println(cr.getRadius());
+        System.out.println(cr.getArea());
+    }
+}
