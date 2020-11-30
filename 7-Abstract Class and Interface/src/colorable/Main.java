709ffc637b32213b31efd2b00473b8adf8d84773
Domia <39hson@gmail.com>
2020-11-30 08:46:16 +0700

    add colorable package

---
+++ 7-Abstract Class and Interface/src/colorable/Main.java
@@ -0,0 +1,9 @@
+package colorable;
+
+public class Main {
+    public static void main(String[] args) {
+        Square sq = new Square(4);
+        System.out.println(sq.getSide());
+        sq.howToColor();
+    }
+}
