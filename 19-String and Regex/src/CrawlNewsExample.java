d8d3a40f4b6309820b8bd1b9721feeee83f8c81a
Domia <39hson@gmail.com>
2021-01-13 15:55:19 +0700

    Add CrawlNewsExample class

--- 19-String and Regex/src/CrawlNewsExample.java
+++ 19-String and Regex/src/CrawlNewsExample.java
@@ -7,22 +7,20 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class CrawlSongExample {
+public class CrawlNewsExample {
     public static void main(String[] args) {
         try {
-            URL url = new URL("https://www.nhaccuatui.com/bai-hat/nhac-tre-moi.html");
+            URL url = new URL("https://dantri.com.vn/the-gioi.htm");
             Scanner scanner = new Scanner(new InputStreamReader(url.openStream()));
             scanner.useDelimiter("\\Z");
             String content = scanner.next();
             scanner.close();
-            content = content.replaceAll("\\n+", "");
-//            System.out.println(content);
-            Pattern p = Pattern.compile("name_song\">(.*?)</a>");
+            content = content.replaceAll("\\s+", " ").replaceAll("&quot;", "\"");
+            Pattern p = Pattern.compile("<a data-utm=\"(.*?)\" class=\"news-item__avatar\" href=\"(.*?)\" title=\"(.*?)\">");
             Matcher m = p.matcher(content);
             while (m.find()) {
-                System.out.println(m.group(0));
+                System.out.println(m.group(3));
             }
-//            System.out.println(m.find());
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (UnsupportedEncodingException e) {
@@ -31,6 +29,6 @@ public class CrawlSongExample {
             e.printStackTrace();
         }
 
+
     }
 }
-
