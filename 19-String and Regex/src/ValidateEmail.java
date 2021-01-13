11bc5a9d95a228e4b3b0cdeb4d75bf6d4b5ee70d
Domia <39hson@gmail.com>
2021-01-13 10:27:21 +0700

    Optimize ValidateEmail class

--- 19-String and Regex/src/ValidateEmail.java
+++ 19-String and Regex/src/ValidateEmail.java
@@ -3,31 +3,28 @@ import java.util.regex.Pattern;
 
 public class ValidateEmail {
     private static Pattern pattern;
-    private Matcher matcher;
-
-    private static final String EMAIL_REGEX =  "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
+    public static final String[] validEmail = new String[]{"a@gmail.com", "ab@yahoo.com", "abc@hotmail.com"};
+    public static final String[] invalidEmail = new String[]{"@gmail.com", "ab@gmail.", "@#abc@gmail.com"};
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
 
     public ValidateEmail() {
         pattern = Pattern.compile(EMAIL_REGEX);
     }
 
     public boolean validate(String regex) {
-        matcher = pattern.matcher(regex);
+        Matcher matcher = pattern.matcher(regex);
         return matcher.matches();
     }
-    private static ValidateEmail emailExample;
-    public static final String[] validEmail = new String[] { "a@gmail.com", "ab@yahoo.com", "abc@hotmail.com" };
-    public static final String[] invalidEmail = new String[] { "@gmail.com", "ab@gmail.", "@#abc@gmail.com" };
 
-    public static void main(String args[]) {
-        emailExample = new ValidateEmail();
+    public static void main(String[] args) {
+        ValidateEmail emailExample = new ValidateEmail();
         for (String email : validEmail) {
-            boolean isvalid = emailExample.validate(email);
-            System.out.println("Email is " + email +" is valid: "+ isvalid);
+            boolean isValid = emailExample.validate(email);
+            System.out.println("Email is " + email + " is valid: " + isValid);
         }
         for (String email : invalidEmail) {
-            boolean isvalid = emailExample.validate(email);
-            System.out.println("Email is " + email +" is valid: "+ isvalid);
+            boolean isValid = emailExample.validate(email);
+            System.out.println("Email is " + email + " is valid: " + isValid);
         }
     }
 }
